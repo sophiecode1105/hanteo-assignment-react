5c3fe399b57ba67958c290a0fd1bfc7ab67572e6
sophie <sophiecode1105@gmail.com>
2024-04-06 15:18:53 +0900

    add: footer component, modify css

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -5,18 +5,22 @@ import Event from "./page/Event/Event";
 import News from "./page/News/News";
 import Store from "./page/Store/Store";
 import Charge from "./page/Charge/Charge";
+import Nav from "./components/Nav/Nav";
+import Footer from "./components/Footer/Footer";
 
 const Routers = () => {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
-        <Route path="/" element={<Chart />} />
+        <Route path="/chart" element={<Chart />} />
         <Route path="/whook" element={<Whook />} />
         <Route path="/event" element={<Event />} />
         <Route path="/news" element={<News />} />
         <Route path="/store" element={<Store />} />
         <Route path="/charge" element={<Charge />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
--- src/page/Chart/Chart.tsx
+++ src/page/Chart/Chart.tsx
@@ -1,13 +1,7 @@
-import Nav from "../../components/Nav/Nav";
 import styles from "./Chart.module.scss";
 
 const Chart = () => {
-  return (
-    <div className={styles.container}>
-      <Nav />
-      오엥?
-    </div>
-  );
+  return <div className={styles.container}>오엥?</div>;
 };
 
 export default Chart;
