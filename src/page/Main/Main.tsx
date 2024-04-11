7b3af574b22de0541de1bd5480b0d13c8d76ce89
sophie <sophiecode1105@gmail.com>
2024-04-11 22:06:34 +0900

    feat: drag nav, fixing infinite loading error

--- src/page/Main/Main.tsx
+++ src/page/Main/Main.tsx
@@ -2,27 +2,30 @@ import { fetchCardData } from "../../assets/data/cardData";
 import Card, { ChartData } from "../../components/Card/Card";
 import Carousel from "../../components/Carousel/Crousel";
 import styles from "./Main.module.scss";
+import loading_gif from "../../assets/img/loading.gif";
 import { useCallback, useEffect, useRef, useState } from "react";
 
 const Main = () => {
   const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
-  const target = useRef<HTMLDivElement | null>(null);
-
   const [cardList, setCardList] = useState<ChartData[]>([]);
+  const target = useRef<HTMLDivElement | null>(null);
 
   const addPages = () => {
     setPage((prev) => prev + 1);
   };
 
   useEffect(() => {
+    //실제로 서버에서 들어오는 데이터처럼 구현하기위하여 dummy data를 fetch하는 함수를 만들어서 사용합니다.
     const fetchData = async () => {
+      setIsLoading(true);
       try {
-        setIsLoading(true);
         const result = await fetchCardData(page);
-        console.log("에?", result);
-        if (result.length === 0) console.log("no more data to fetch");
-        else setCardList((prevList) => [...prevList, ...result]);
+        if (result.length === 0) {
+          //아무것도 없을시에는 target.current를 null로 만들어서 더이상 fetch를 하지 않고, observer가 돌지않도록 합니다.
+          target.current = null;
+          return;
+        } else setCardList((prevList) => [...prevList, ...result]);
       } catch (error) {
         console.error("Error fetching card data:", error);
       } finally {
@@ -33,7 +36,7 @@ const Main = () => {
     fetchData();
   }, [page]);
 
-  const callback = useCallback(
+  const observerCallback = useCallback(
     (entries: IntersectionObserverEntry[]) => {
       if (entries[0].isIntersecting && !isLoading) {
         addPages();
@@ -42,12 +45,13 @@ const Main = () => {
     [isLoading]
   );
 
+  //무한스크롤을 위한 Observer를 사용합니다.
   useEffect(() => {
     if (!target.current) return;
-    const observer = new IntersectionObserver(callback);
+    const observer = new IntersectionObserver(observerCallback);
     observer.observe(target.current);
     return () => observer.disconnect();
-  }, [callback, target]);
+  }, [observerCallback, target]);
 
   return (
     <div className={styles.container}>
@@ -55,7 +59,12 @@ const Main = () => {
       {cardList?.map((card, idx) => (
         <Card key={`card-${idx}`} chartData={card} />
       ))}
-      <div ref={target}>Loading...</div>
+      <div ref={target}></div>
+      {isLoading && (
+        <div className={styles.loading}>
+          <img src={loading_gif} alt="loading" />
+        </div>
+      )}
     </div>
   );
 };
